040a35e0f211009bc8a683edaeab3de897a1da4c
dissisor <serzh.gordienko.1997@gmail.com>
2024-06-04 02:23:12 +0300

    просто пара маршрутов

--- ProjectManager.Api/Program.cs
+++ ProjectManager.Api/Program.cs
@@ -1,6 +1,13 @@
-using ProjectManager;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+using ProjectManager;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -9,7 +16,6 @@ var configuration = new ConfigurationBuilder()
 
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-// var ctx = new ApplicationContext(connectionString);
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,6 +23,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+	{
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+        };
+	}
+);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -28,21 +50,99 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-var projectsApi = app.MapGroup("/projects").WithOpenApi();
+var projectsApi = app.MapGroup("/projects").WithOpenApi()
+    .WithTags("Projects");
+var usersApi = app.MapGroup("/users").WithOpenApi()
+    .WithTags("Users");
+var loginApi = app.MapGroup("/login").WithOpenApi()
+    .WithTags("Auth");
 
-projectsApi.MapGet("/", async (ApplicationContext ctx) => await ctx.Projects.ToListAsync()) 
-.WithName("GetProjects")
-.WithOpenApi();
-
-projectsApi.MapPost("/", async (Project project, ApplicationContext ctx) =>
+loginApi.MapPost("/{username}", (string username) =>
 {
+    var claims = new List<Claim> {new Claim(ClaimTypes.Name, username) };
+    var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    return new JwtSecurityTokenHandler().WriteToken(jwt);
+})
+    .WithName("Login")
+    .WithOpenApi();
+
+projectsApi.MapGet("/", async (ApplicationContext ctx) => await ctx.Projects.ToListAsync())
+    .WithName("GetProjects")
+    .WithOpenApi();
+
+projectsApi.MapPost("/", async (string name, bool? isActive, ApplicationContext ctx) =>
+{
+    // TODO: работает только с 32-битными числами. Обобщить для
+    // кодов произвольного размера.
+
+    var nextKey = (ctx.Projects.AsEnumerable().Select(
+  		 p => Int32.Parse(p.Code)).Max() + 1).ToString();
+    var project = new Project(name, nextKey, isActive ?? true);
     ctx.Projects.Add(project);
     await ctx.SaveChangesAsync();
     return Results.Created($"/projects/{project.Code}", project);
 })
-.WithName("AddProject")
-.WithOpenApi();
+    .WithName("AddProject")
+    .WithOpenApi();
+
+projectsApi.MapGet("/{code:regex([0-9]+)}", async (string code, ApplicationContext ctx) =>
+    await ctx.Projects.FindAsync(code)
+        is Project proj
+            ? Results.Ok(proj)
+            : Results.NotFound())
+    .WithName("GetProjectByCode")
+    .WithOpenApi();
+
+projectsApi.MapGet("/{code:regex([0-9]+)}/tasks", async (string code, ApplicationContext ctx) =>
+{
+    var proj = await ctx.Projects.FindAsync(code);
+    if (proj == null) return Results.NotFound();
+    return Results.Ok(
+        ctx.Tasks.Where(t => t.Project == proj).ToListAsync()
+    );
+})
+    .WithName("GetProjectTasks")
+    .WithOpenApi();
+
+usersApi.MapGet("/", async (ApplicationContext ctx) => await ctx.Users.ToListAsync())
+    .WithName("GetUsers")
+    .WithOpenApi();
+
+usersApi.MapPost("/", async (string name, ApplicationContext ctx) =>
+{
+    var nextId = ctx.Users.Select(u => u.Id).Max() + 1;
+    var user = new User(nextId, name);
+    ctx.Users.Add(user);
+    await ctx.SaveChangesAsync();
+    return Results.Created($"/users/{user.Id}", user);
+})
+    .WithName("AddUser")
+    .WithOpenApi();
+
+usersApi.MapGet("/{id:int}", async (int id, ApplicationContext ctx) =>
+    await ctx.Users.FindAsync(id)
+        is User user
+            ? Results.Ok(user)
+            : Results.NotFound())
+    .WithName("GetUserByiD")
+    .WithOpenApi();
 
 app.Run();
+
+public class AuthOptions
+{
+    public const string ISSUER = "KSU";
+    public const string AUDIENCE = "PlaceHolderAudience";
+    const string KEY = "mysupersecret_secretsecretsecretkey!123";
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
 
